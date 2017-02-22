275c698a4b2b370c65ab7862e3c76e4d45ff805d
David Zeng <davidzeng@Davids-MacBook-Pro.local>
2017-02-21 22:39:45 -0500

    small fix

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -1,30 +1,28 @@
-import tensorflow as tf
+def cnn(features, labels, mode):
+    # Input layer
+    input_layer = tf.reshape(features, [-1, 28, 28, 1])
 
-# Load MNIST data
-from tensorflow.example.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    # Convolution layer 1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=5,
+        padding="same",
+        activation=tf.nn.relu)
 
-# Convolution layer 1
-conv1 = tf.layers.conv2d(
-    inputs=input_layer,
-    filters=32,
-    kernel_size=5,
-    padding="same",
-    activation=tf.nn.relu)
+    # Pool layer 1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size = [2, 2], strides = 2)
 
-# Pool layer 1
-pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size = [2, 2], strides = 2)
+    # Convolution layer 2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=5,
+        padding="same",
+        activation=tf.nn.relu)
 
-# Convolution layer 2
-conv2 = tf.layers.conv2d(
-    inputs=pool1,
-    filters=64,
-    kernel_size=5,
-    padding="same",
-    activation=tf.nn.relu)
+    # Pool layer 2
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size = [2, 2], strides = 2)
 
-# Pool layer 2
-pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size = [2, 2], strides = 2)
-
-# Dense layer
+    # Dense layer
 
