9225b71c85c5488351721f8d28df9cd98562a491
David Zeng <davidzeng@Davids-MacBook-Pro.local>
2017-02-22 21:46:51 -0500

    added save function

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -59,14 +59,17 @@ b_fc2 = bias_variable([10])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-# Train and Evaluate the Model
+# Cost function and backward prop
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# Start run
-sess.run(tf.initialize_all_variables())
+# Saver
+saver = tf.train.Saver()
+
+# Train steps
+sess.run(tf.global_variables_initializer())
 for i in range(20000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
@@ -74,4 +77,10 @@ for i in range(20000):
         print("step %d, training accuracy %g" % (i, train_accuracy))
     train_step.run(feed_dict = {x: batch[0], y_: batch[1], keep_prob: 0.5})
 
+# Save
+save_path = saver.save(sess, "model.ckpt")
+print("Model saved as: %s" % save_path)
+
+# Test step
 print("test accuracy %g" % accuracy.eval(feed_dict = {x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
